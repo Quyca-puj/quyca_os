Quyca-puj/STBlockly

Create RobotConfig and remove warnings
There is a global RobotConfig, which can be accessed through config.
Could be a good idea to move more vars there.

Arduino/ROBOTQUEMES/config.cpp
@@ -1,0 +1,21 @@
+#include "config.h"
+
+RobotConfig config;
+
+RobotConfig::RobotConfig() {
+	setDefaults();
+}
+
+void RobotConfig::setDefaults() {
+	config[ConfigOptions::SPEED] = 50;
+	config[ConfigOptions::SPEED_OFFSET_RIGHT] = 90;
+	config[ConfigOptions::SPEED_OFFSET_LEFT] = 90;
+}
+
+int RobotConfig::getConfig(ConfigOptions co) {
+	return config[co];
+}
+
+void RobotConfig::setConfig(ConfigOptions co, int new_value) {
+	config[co] = new_value;
+}

Arduino/ROBOTQUEMES/config.h
@@ -1,0 +1,20 @@
+#ifndef ROBOT_CONFIG_H_
+#define ROBOT_CONFIG_H_
+
+enum ConfigOptions {
+	SPEED,
+	SPEED_OFFSET_RIGHT,
+	SPEED_OFFSET_LEFT,
+};
+
+class RobotConfig {
+	int config[3];
+public:
+	RobotConfig();
+	void setDefaults();
+	int getConfig(ConfigOptions co);
+	void setConfig(ConfigOptions co, int new_value);
+};
+
+extern RobotConfig config;
+#endif

Arduino/ROBOTQUEMES/motorSpeed.cpp
@@ -1,16 +1,13 @@
 #include "motorSpeed.h"
 
-//zero value motors
-const int SPEED_DEGREE_CERO_LM=90;
-const int SPEED_DEGREE_CERO_RM=90;
 /* Define again Servo*/
 Servo rightServo;
 Servo leftServo;
 void setupmotor(){
   leftServo.attach(LEFT_MOTOR);
   rightServo.attach(RIGHT_MOTOR);
-  leftServo.write(SPEED_DEGREE_CERO_LM);
-  rightServo.write(SPEED_DEGREE_CERO_RM);
+  leftServo.write(config.getConfig(ConfigOptions::SPEED_OFFSET_LEFT));
+  rightServo.write(config.getConfig(ConfigOptions::SPEED_OFFSET_RIGHT));
 }
 void setSpeedsMotor(int leftSp,int rigthSp){
   //***********************************************************************************
@@ -24,24 +21,24 @@ void setSpeedsMotor(int leftSp,int rigthSp){
       if (leftSp >= 0)
       {
         // Scale it to use it with the servo (value between 0 and 90 -> 0 to 90).
-        leftS = SPEED_DEGREE_CERO_LM - map(leftSp, 0, 255, 0, 90);
+        leftS = config.getConfig(ConfigOptions::SPEED_OFFSET_LEFT) - map(leftSp, 0, 255, 0, 90);
       }
       else
       {
         // Scale it to use it with the servo (value between 0 and 90 -> 90 to 180).
-        leftS = map( (leftSp * -1) , 0, 255, 0, 90) + SPEED_DEGREE_CERO_LM;
+        leftS = map( (leftSp * -1) , 0, 255, 0, 90) + config.getConfig(ConfigOptions::SPEED_OFFSET_LEFT);
       }
     
       // Check if speed more than 0.
       if (rigthSp >= 0)
       {
         // Scale it to use it with the servo (value between 0 and 90 -> 90 to 180).
-        rigthS =  map(rigthSp, 0, 255, 0, 90) + SPEED_DEGREE_CERO_RM;
+        rigthS =  map(rigthSp, 0, 255, 0, 90) + config.getConfig(ConfigOptions::SPEED_OFFSET_RIGHT);
       }
       else
       {
         // Scale it to use it with the servo (value between 0 and 90 -> 0 to 90).
-        rigthS = SPEED_DEGREE_CERO_RM - map( (rigthSp * -1) , 0, 255, 0, 90);
+        rigthS = config.getConfig(ConfigOptions::SPEED_OFFSET_RIGHT) - map( (rigthSp * -1) , 0, 255, 0, 90);
       }
     
       // Set the results speeds.

Quyca-puj/quyca_os

config.cpp
@@ -1,0 +1,21 @@
+#include "config.h"
+
+RobotConfig config;
+
+RobotConfig::RobotConfig() {
+	setDefaults();
+}
+
+void RobotConfig::setDefaults() {
+	config[ConfigOptions::SPEED] = 50;
+	config[ConfigOptions::SPEED_OFFSET_RIGHT] = 90;
+	config[ConfigOptions::SPEED_OFFSET_LEFT] = 90;
+}
+
+int RobotConfig::get(ConfigOptions co) {
+	return config[co];
+}
+
+void RobotConfig::set(ConfigOptions co, int new_value) {
+	config[co] = new_value;
+}

config.h
@@ -1,0 +1,20 @@
+#ifndef ROBOT_CONFIG_H_
+#define ROBOT_CONFIG_H_
+
+enum ConfigOptions {
+	SPEED,
+	SPEED_OFFSET_RIGHT,
+	SPEED_OFFSET_LEFT,
+};
+
+class RobotConfig {
+	int config[3];
+public:
+	RobotConfig();
+	void setDefaults();
+	int get(ConfigOptions co);
+	void set(ConfigOptions co, int new_value);
+};
+
+extern RobotConfig config;
+#endif

motorSpeed.cpp
@@ -1,16 +1,13 @@
 #include "motorSpeed.h"
 
-//zero value motors
-const int SPEED_DEGREE_CERO_LM=90;
-const int SPEED_DEGREE_CERO_RM=90;
 /* Define again Servo*/
 Servo rightServo;
 Servo leftServo;
 void setupmotor(){
   leftServo.attach(LEFT_MOTOR);
   rightServo.attach(RIGHT_MOTOR);
-  leftServo.write(SPEED_DEGREE_CERO_LM);
-  rightServo.write(SPEED_DEGREE_CERO_RM);
+  leftServo.write(config.get(ConfigOptions::SPEED_OFFSET_LEFT));
+  rightServo.write(config.get(ConfigOptions::SPEED_OFFSET_RIGHT));
 }
 void setSpeedsMotor(int leftSp,int rigthSp){
   //***********************************************************************************
@@ -24,24 +21,24 @@ void setSpeedsMotor(int leftSp,int rigthSp){
       if (leftSp >= 0)
       {
         // Scale it to use it with the servo (value between 0 and 90 -> 0 to 90).
-        leftS = SPEED_DEGREE_CERO_LM - map(leftSp, 0, 255, 0, 90);
+        leftS = config.get(ConfigOptions::SPEED_OFFSET_LEFT) - map(leftSp, 0, 255, 0, 90);
       }
       else
       {
         // Scale it to use it with the servo (value between 0 and 90 -> 90 to 180).
-        leftS = map( (leftSp * -1) , 0, 255, 0, 90) + SPEED_DEGREE_CERO_LM;
+        leftS = map( (leftSp * -1) , 0, 255, 0, 90) + config.get(ConfigOptions::SPEED_OFFSET_LEFT);
       }
     
       // Check if speed more than 0.
       if (rigthSp >= 0)
       {
         // Scale it to use it with the servo (value between 0 and 90 -> 90 to 180).
-        rigthS =  map(rigthSp, 0, 255, 0, 90) + SPEED_DEGREE_CERO_RM;
+        rigthS =  map(rigthSp, 0, 255, 0, 90) + config.get(ConfigOptions::SPEED_OFFSET_RIGHT);
       }
       else
       {
         // Scale it to use it with the servo (value between 0 and 90 -> 0 to 90).
-        rigthS = SPEED_DEGREE_CERO_RM - map( (rigthSp * -1) , 0, 255, 0, 90);
+        rigthS = config.get(ConfigOptions::SPEED_OFFSET_RIGHT) - map( (rigthSp * -1) , 0, 255, 0, 90);
       }
     
       // Set the results speeds.